83d2e4e16d75ae593bd25d93fe81841928a06785
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-03-25 19:50:00 +0600

    facebook authentication done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { FacebookAuthProvider, getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import app from './firebase.init';
 import { useState } from 'react';
 
@@ -8,10 +8,23 @@ const auth = getAuth(app);
 function App() {
     const [user, setUser] = useState({});
     const googleProvider = new GoogleAuthProvider();
+    const facebookProvider = new FacebookAuthProvider();
     const githubProvider = new GithubAuthProvider();
 
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, googleProvider)
+            .then((result) => {
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+            })
+            .catch((error) => {
+                console.error('error', error);
+            });
+    };
+
+    const handleFacebookSignIn = () => {
+        signInWithPopup(auth, facebookProvider)
             .then((result) => {
                 const user = result.user;
                 setUser(user);
@@ -50,6 +63,7 @@ function App() {
             ) : (
                 <>
                     <button onClick={handleGoogleSignIn}>Google Sign In </button>
+                    <button onClick={handleFacebookSignIn}>Facebook Sign In</button>
                     <button onClick={handleGithubSignIn}>Github sign In</button>
                 </>
             )}
